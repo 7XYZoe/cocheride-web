d1aa5e99f5a61010120ab25a639ec871d0925392
Zoe <7xyzoe@gmail.com>
2023-02-03 13:47:53 +0100

    refactoring pages + other new pages

---
+++ comp/footer.php
@@ -0,0 +1,99 @@
+<footer class="bg-dark text-inverse" data-cues="fadeIn">
+    <div class="container pt-17 pt-md-19 pb-8 pb-md-10">
+      <div class="row gy-6 gy-lg-0">
+        <div class="col-md-6 col-lg-6" data-cues="fadeIn">
+          <div class="widget">
+            <img class="mb-4" src="assets/img/logo-light.png" srcset="assets/img/logo-light@2x.png 2x" alt="" />
+            <address class="pe-xl-15 pe-xxl-17">Coche Ride Transportation and Logistics have developed an all-in-one robust logistics ecosystem that simplifies the distribution of goods by creating an efficient logistics experience for cargo owners and distributors who are burdened with the task of moving their goods within the country.</address>
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-3 col-lg-3">
+          <div class="widget">
+            
+            <!-- /.social -->
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+        <div class="col-md-3 col-lg-3" data-cues="fadeIn">
+          <div class="widget">
+            <h4 class="widget-title text-white mb-3">Get in Touch</h4>
+            <a target="_blank" href="mailto:contact@cocheride.org" class="link-inverse">contact@cocheride.org</a>
+            <br /> +234 811 577 9007
+            <nav class="nav social social-white">
+              <a target="_blank" href="https://twitter.com/coche_ride"><i class="uil uil-twitter"></i></a>
+              <a target="_blank" href="https://web.facebook.com/cocheride"><i class="uil uil-facebook-f"></i></a>
+              <a target="_blank" href="https://www.instagram.com/cocheride/"><i class="uil uil-instagram"></i></a>
+              <a target="_blank" href="https://www.linkedin.com/company/cocheride-technology-logistics-ltd/"><i class="uil uil-linkedin"></i></a>
+            </nav>
+            <p></p>
+
+            <p class="mb-4">© 2023 Coche Ride. <br class="d-none d-lg-block" />All rights reserved.</p>
+
+          </div>
+          <!-- /.widget -->
+        </div>
+        <!-- /column -->
+      </div>
+      <!--/.row -->
+    </div>
+    <!-- /.container -->
+  </footer>
+  <div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+  </div>
+
+<!-- Modal section starts -->
+
+<div class="modal fade" id="modal-quotation" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered modal-sm">
+    <div class="modal-content text-center">
+      <div class="modal-body">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <h2 class="mb-3 text-start">Get a quick trip quotation now</h2>
+        <p class="lead mb-6 text-start">Get a truck trip estimate rightaway!</p>
+        <form class="text-start mb-3">
+          <div class="form-floating mb-4">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Truck Type</option>
+              <option value="1">5 Ton</option>
+              <option value="2">10 Ton</option>
+              <option value="3">20 Ton</option>
+              <option value="4">30 Ton</option>
+            </select>
+          </div>
+          <div class="form-floating mb-4">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Number of Trucks</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div class="form-floating password-field mb-4">
+            <input id="textInputExample" type="text" class="form-control" placeholder="Pickup Location">
+            <label for="textInputExample">Pickup Location</label>
+          </div>
+          <div class="form-floating password-field mb-4">
+            <input id="textInputExample" type="text" class="form-control" placeholder="Delivery Location">
+            <label for="textInputExample">Delivery Location</label>
+          </div>
+          <a class="btn btn-primary rounded-pill btn-login w-100 mb-2">Get Quotation!</a>
+        </form>
+        <!-- /form -->
+      </div>
+      <!--/.modal-content -->
+    </div>
+    <!--/.modal-body -->
+  </div>
+  <!--/.modal-dialog -->
+</div>
+<!--/.modal -->
+
+<!-- Modal section ends -->
